4812ecdad56bba204052aff4b91a859053e2cca4
Simon Bannister <sibannister@yahoo.co.uk>
2019-09-19 07:18:11 +0100

    Forgot to add verifying observer

--- cs-silverbars/src/test/java/cs/exercise/OrderBoardTests.java
+++ cs-silverbars/src/test/java/cs/exercise/OrderBoardTests.java
@@ -28,16 +28,19 @@ public class OrderBoardTests {
     private Order secondSellOrder = new Order("user2", secondQuantity, highestPrice, OrderType.SELL);
     private Order thirdSellOrder = new Order("user3", thirdQuantity, midPrice, OrderType.SELL);
     private Order fourthSellOrder = new Order("user4", fourthQuantity, lowestPrice, OrderType.SELL);
+    private OrderBoardSummary orderBoardSummary;
 
     @Before
     public void setUp() {
-        orderBoard = new OrderBoard();
+        orderBoardSummary = mock(OrderBoardSummary.class);
+        orderBoard = new OrderBoard("TEST", orderBoardSummary);
     }
 
     @Test
     public void shouldRegisterOrder() {
         assertThat(orderBoard.registerOrder(firstSellOrder), is(true));
         assertThat(orderBoard.getOrders(), contains(firstSellOrder));
+        verify(orderBoardSummary, times(1)).update(orderBoard, null);
     }
 
     @Test
@@ -46,6 +49,7 @@ public class OrderBoardTests {
         assertThat(orderBoard.getOrders(), contains(firstSellOrder));
         assertThat(orderBoard.registerOrder(firstSellOrder), is(false));
         assertThat(orderBoard.getOrders(), contains(firstSellOrder));
+        verify(orderBoardSummary, times(1)).update(orderBoard, null);
     }
 
     @Test
@@ -66,6 +70,7 @@ public class OrderBoardTests {
         assertThat(orderBoard.getOrders(), contains(firstSellOrder));
         assertThat(orderBoard.cancelOrder(firstSellOrder), is(true));
         assertThat(orderBoard.getOrders(), not(contains(firstSellOrder)));
+        verify(orderBoardSummary, times(2)).update(orderBoard, null);
     }
 
     @Test
@@ -74,6 +79,7 @@ public class OrderBoardTests {
         assertThat(orderBoard.getOrders(), contains(firstSellOrder));
         assertThat(orderBoard.cancelOrder(secondSellOrder), is(false));
         assertThat(orderBoard.getOrders(), contains(firstSellOrder));
+        verify(orderBoardSummary, times(1)).update(orderBoard, null);
     }
 
     @Test
@@ -84,5 +90,27 @@ public class OrderBoardTests {
         orderBoard.registerOrder(fourthSellOrder);
 
         assertThat(orderBoard.getOrders(), containsInAnyOrder(firstSellOrder, secondSellOrder, thirdSellOrder, fourthSellOrder));
+        verify(orderBoardSummary, times(4)).update(orderBoard, null);
+    }
+
+    @Test
+    public void shouldReturnOrderBoardSummaryWithDefaultConstructor() {
+        OrderBoard orderBoard = new OrderBoard();
+        assertThat(orderBoard.getSummaryInformation(), instanceOf(OrderBoardSummary.class));
+    }
+
+    @Test
+    public void shouldReturnOrderBoardSummaryProvided() {
+        String boardName = "TEST";
+        OrderBoardSummary test = new OrderBoardSummary(boardName);
+        OrderBoard orderBoard = new OrderBoard(boardName, test);
+        assertThat(orderBoard.getSummaryInformation(), is(test));
+        assertThat(orderBoard.getName(), is(boardName));
+    }
+
+    @Test
+    public void shouldThrowExceptionForNullOrderBoardSummary() {
+        expectedException.expect(IllegalArgumentException.class);
+        new OrderBoard("TEST", null);
     }
 }
